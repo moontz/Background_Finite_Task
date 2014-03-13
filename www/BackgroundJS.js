d48c0975c17913f44bb33bdb1bf504d127fc31cf
moontz <brian.munter@gmail.com>
2014-03-12 20:18:17 -0500

    added js functions for remaining items

--- www/BackgroundJS.js
+++ www/BackgroundJS.js
@@ -1,5 +1,13 @@
 
 
+LockBackgroundTime = function(seconds, callback) {
+    cordova.exec(callback, null, "BackgroundJS", "lockBackgroundTime", []);
+};
+
+UnlockBackgroundTime = function(seconds, callback) {
+    cordova.exec(callback, null, "BackgroundJS", "unlockBackgroundTime", []);
+};
+
 SetBackgroundSeconds = function(seconds, callback) {
     cordova.exec(callback, null, "BackgroundJS", "setBackgroundSeconds", [seconds]);
 };
